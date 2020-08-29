80afe4a45b8639b5c26ee52ac8e497b85e1c45cb
1-2-1-2 <v.sangwan9876@gmail.com>
2020-08-29 20:40:22 +0530

    promise and passing parameters to promise

--- jQuery/5.promise.js
+++ jQuery/5.promise.js
@@ -3,9 +3,13 @@ var userLoggedIn = true;
 var promise = new Promise((resolve, reject) => {
     setTimeout(()=>{
         if(userLoggedIn){
+
+            //promise is resolved
             resolve();
 
         }else{
+
+            //promise is rejected
             reject();
         }
     },1000);
@@ -15,4 +19,30 @@ promise.then( ()=>{
     console.log('User Logged In');
 }).catch(()=>{
     console.log("User not Logged In")
+})
+
+
+//Passing Parameters to promise
+
+var userLoggedIn = true;
+
+function checkUserLogIn(){
+var promise = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+        if(userLoggedIn){
+            //this  value is send to then function in successMsg and print it.
+            resolve("User Logged In");
+
+        }else{
+            reject("User Logged Out");
+        }
+    },1000);
+});
+    return promise;
+}
+
+promise.then( function (successMsg){
+    console.log(successMsg);
+}).catch( function(successMsg){
+    console.log(successMsg);
 })
